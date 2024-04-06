2c22926e0f2573b06c1a6ce58918650fd89331bc
KikeCrux <kikke8508@gmail.com>
2024-04-06 17:16:02 -0600

    Pagina de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterOutlet,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
