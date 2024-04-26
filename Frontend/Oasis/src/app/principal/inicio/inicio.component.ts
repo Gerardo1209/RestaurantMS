63bf70a945968a85f513004f9a8cd5bb36ec1cc4
KikeCrux <kikke8508@gmail.com>
2024-04-25 21:22:22 -0600

    Responsive design Co-authored-by: Gerardo Femat Delgado

--- Frontend/Oasis/src/app/principal/inicio/inicio.component.ts
+++ Frontend/Oasis/src/app/principal/inicio/inicio.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
+  cards = [
+    {
+      imageUrl: '../../../assets/png/1.png',
+      title: 'Producto 1',
+      text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+      imageUrl: '../../../assets/png/2.png',
+      title: 'Producto 2',
+      text: 'This is a short card.'
+    },
+    {
+      imageUrl: '../../../assets/png/3.png',
+      title: 'Producto 3',
+      text: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
+    },
+    {
+      imageUrl: '../../../assets/png/5.png',
+      title: 'Producto 4',
+      text: 'This is a short card.'
+    },
+    {
+      imageUrl: '../../../assets/png/6.png',
+      title: 'Producto 5',
+      text: 'This is a longer card with supporting text below as a natural lead-in to additional content.'
+    },
+    {
+      imageUrl: '../../../assets/png/1.png',
+      title: 'Producto 6',
+      text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
 
+  ];
 }
