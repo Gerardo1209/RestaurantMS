38cbdfb5636a211549cec642a006d293314fac38
Shadow1900 <118683823+Shadow1900@users.noreply.github.com>
2024-04-27 13:47:03 -0600

    Co-authored-by: Gerardo Femat Delgado <Gerardo1209@users.noreply.github.com>

---
+++ Backend/src/Auth/sql.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+const config = {
+    server: 'localhost',
+    database: 'Restaurante_Final_1',
+    user: 'sa', // Nombre de usuario
+    password: '12345', // Contraseña
+    options: {
+        encrypt: false, // Dependiendo de tu configuración de SQL Server, puede que necesites cambiar este valor
+        trustServerCertificate: false // Dependiendo de tu configuración de SQL Server, puede que necesites cambiar este valor
+    }
+};
+
+var pool = new sql.ConnectionPool(config)
+    .connect()
+    .then(pool => {
+        console.log("Base de datos conectada");
+        return pool;
+    })
+    .catch((error) => {
+        console.log("La conexión a la base de datos fallo");
+    })
+
+module.exports = {
+    sql, pool
+}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,44 +1,14 @@
 const express = require('express');
-const sql = require('mssql');
-
+const db = require('./Auth/sql')
 const app = express();
 
-// Configuración de la conexión a la base de datos
-const config = {
-    server: 'localhost',
-    database: 'Restaurante_Final',
-    user: 'sa', // Nombre de usuario
-    password: '12345', // Contraseña
-    options: {
-        encrypt: false, // Dependiendo de tu configuración de SQL Server, puede que necesites cambiar este valor
-        trustServerCertificate: false // Dependiendo de tu configuración de SQL Server, puede que necesites cambiar este valor
-    }
-};
 
-// Función para conectar a la base de datos y ejecutar una consulta
-async function conectarYConsultar() {
-    try {
-        // Conexión a la base de datos
-        await sql.connect(config);
-
-        // Consulta SQL
-        const resultado = await sql.query`SELECT * FROM Cliente`;
-
-        return resultado.recordset; // Devuelve los registros obtenidos
-
-    } catch (error) {
-        throw new Error('Error al conectar a la base de datos: ' + error.message);
-    } finally {
-        // Cierra la conexión a la base de datos
-        await sql.close();
-    }
-}
-
-// Ruta para manejar las solicitudes HTTP
+//Ruta para manejar las solicitudes HTTP
 app.get('/clientes', async (req, res) => {
     try {
-        const clientes = await conectarYConsultar();
-        res.json(clientes); // Devuelve los registros como JSON
+        const pool = await db.pool;
+        const result = await pool.request().query('SELECT * FROM cliente');
+        res.json(result.recordset); // Devuelve los registros como JSON
     } catch (error) {
         res.status(500).json({ error: error.message }); // Devuelve un error HTTP 500 en caso de error
     }
