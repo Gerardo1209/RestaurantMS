b700e7b5daada9f8522f97b432f018496d922737
Gerardo Femat Delgado <fematdelgado.github@gmail.com>
2024-04-27 23:21:33 -0600

    Pedidos

---
+++ Backend/src/routes/pedidos.js
@@ -0,0 +1,10 @@
+/**
+ * En esta ruta se especifican todas las operaciones necesarias para manejar los pedidos
+ */
+const express = require('express');
+const router = express.Router();
+const db = require('../Auth/sql')
+const authManager = require('../Auth/user_permision');
+
+
+// VISUALIZAR PEDIDOS DE MESEROS, COCINEROS Y BARTENDERS
