11b274d8cffa2efc653f9684519b08eee806645c
ArturoV2001 <ArturoV2001@outlook.com>
2024-04-29 19:28:11 -0600

    creacion de inicio de sesion

--- Frontend/Oasis/src/app/app.routes.ts
+++ Frontend/Oasis/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { InicioComponent } from './principal/inicio/inicio.component';
 import { MenuComponent } from './principal/menu/menu.component';
 import { PreguntasComponent } from './principal/preguntas/preguntas.component';
 import { UbicacionComponent } from './principal/ubicacion/ubicacion.component';
+import { IniciarsesionComponent } from './sistema/iniciarsesion/iniciarsesion.component';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,10 @@ export const routes: Routes = [
     {
         path: 'ubicacion',
         component: UbicacionComponent,
+    },
+    {
+        path: 'iniciarsesion',
+        component: IniciarsesionComponent,
     },
     {
       path: '',
