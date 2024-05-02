cd7003dc66253b59825f7f958190bc20df472332
ArturoV2001 <ArturoV2001@outlook.com>
2024-05-01 21:26:33 -0600

    Adicion de formulario a login

--- Frontend/Oasis/src/app/sistema/iniciarsesion/iniciarsesion.component.ts
+++ Frontend/Oasis/src/app/sistema/iniciarsesion/iniciarsesion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-iniciarsesion',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './iniciarsesion.component.scss'
 })
 export class IniciarsesionComponent {
+  email:string;
+  password:string;
 
+  constructor(){
+    this.email = "";
+    this.password = "";
+  }
+
+  onSubmit(form:NgForm){
+    if(form.valid){
+      this.email = form.value.email;
+      this.password = form.value.password;
+    }
+  }
+  
 }
