d4c9d95714dcb6d931eedd44ec8606da8241c831
ArturoV2001 <ArturoV2001@outlook.com>
2024-05-04 13:41:33 -0600

    Adicion de formulario y de verificacion en reservacion

--- Frontend/Oasis/src/app/principal/reservacion/reservacion.component.ts
+++ Frontend/Oasis/src/app/principal/reservacion/reservacion.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker'
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reservacion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule,],
   templateUrl: './reservacion.component.html',
   styleUrl: './reservacion.component.scss'
 })
 export class ReservacionComponent {
 
+  showCurp: boolean;
+  formNuevaReservacion: FormGroup = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    correo: new FormControl('', [Validators.email]),
+    apellidoPaterno: new FormControl('', [Validators.required]),
+    apellidoMaterno: new FormControl('', [Validators.required]),
+    telefono: new FormControl('', [Validators.required]),
+    curp: new FormControl(''),
+  })
+  formVerificarReservacion: FormGroup = new FormGroup({
+    correo: new FormControl('', [Validators.email]),
+    contrasena: new FormControl('', [Validators.required]),
+  })
+
+  constructor() {
+    this.showCurp = false;
+  }
+
+  onSubmit(opc: number) {
+    if (opc == 1) {
+      console.log("reservar")
+    } else {
+      console.log("verifacr")
+    }
+  }
 }
